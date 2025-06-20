0545a5392aa071a62363e9a1079a8f1a236c3508
Profitro1ka <102919930+Profitro1ka@users.noreply.github.com>
2025-06-20 21:37:49 +0500

    dsadsa

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -65,7 +65,7 @@ const OrganizationSettings = () => {
         id: currentOrganization.id,
         name,
         description,
-        image: undefined,
+        image: image,
         deletedAt: new Date().toISOString(),
       });
     },
